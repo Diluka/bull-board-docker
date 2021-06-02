4f22b41521de73ea302facd8df42d6fc2981bdc4
Diluka <pmars42@gmail.com>
2021-06-02 16:57:24 +0800

    feat: upgrade bull-board to 3.1.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-const {createBullBoard} = require('bull-board');
-const {BullAdapter} = require('bull-board/bullAdapter')
-const {BullMQAdapter} = require('bull-board/bullMQAdapter')
+const {createBullBoard} = require("@bull-board/api");
+const {BullAdapter} = require("@bull-board/api/bullAdapter");
+const {BullMQAdapter} = require("@bull-board/api/bullMQAdapter");
+const {ExpressAdapter} = require('@bull-board/express');
 const Queue = require('bull');
 const bullmq = require('bullmq');
 const express = require('express');
@@ -14,33 +15,37 @@ const {authRouter} = require('./login');
 const config = require('./config');
 
 const redisConfig = {
-	redis: {
-		port: config.REDIS_PORT,
-		host: config.REDIS_HOST,
-		db: config.REDIS_DB,
-		...(config.REDIS_PASSWORD && {password: config.REDIS_PASSWORD}),
-		tls: config.REDIS_USE_TLS === 'true',
-	},
+  redis: {
+    port: config.REDIS_PORT,
+    host: config.REDIS_HOST,
+    db: config.REDIS_DB,
+    ...(config.REDIS_PASSWORD && {password: config.REDIS_PASSWORD}),
+    tls: config.REDIS_USE_TLS === 'true',
+  },
 };
 
 const client = redis.createClient(redisConfig.redis);
-const {router, setQueues} = createBullBoard([]);
+const serverAdapter = new ExpressAdapter();
+const {setQueues} = createBullBoard({queues: [], serverAdapter});
+const router = serverAdapter.getRouter();
 
 client.KEYS(`${config.BULL_PREFIX}:*`, (err, keys) => {
-	const uniqKeys = new Set(keys.map(key => key.replace(/^.+?:(.+?):.+?$/, '$1')));
-	const queueList = Array.from(uniqKeys).sort().map(
-		(item) => {
-			if (config.BULL_VERSION === 'BULLMQ') {
-				return new BullMQAdapter(new bullmq.Queue(item, {connection: redisConfig.redis}));
-			}
+  const uniqKeys = new Set(keys.map(key => key.replace(/^.+?:(.+?):.+?$/, '$1')));
+  const queueList = Array.from(uniqKeys).sort().map(
+    (item) => {
+      if (config.BULL_VERSION === 'BULLMQ') {
+        return new BullMQAdapter(new bullmq.Queue(item, {connection: redisConfig.redis}));
+      }
 
-			return new BullAdapter(new Queue(item, redisConfig));
-		}
-	);
+      return new BullAdapter(new Queue(item, redisConfig));
+    }
+  );
 
-	setQueues(queueList);
-	console.log('done!')
+  setQueues(queueList);
+  console.log('done!');
 });
+
+serverAdapter.setBasePath(config.PROXY_PATH);
 
 const app = express();
 
@@ -48,18 +53,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 if (app.get('env') !== 'production') {
-	console.log('bull-board condig:', config);
-	const morgan = require('morgan');
-	app.use(morgan('combined'));
+  console.log('bull-board condig:', config);
+  const morgan = require('morgan');
+  app.use(morgan('combined'));
 }
-
-app.use((req, res, next) => {
-	if (config.PROXY_PATH) {
-		req.proxyUrl = config.PROXY_PATH;
-	}
-
-	next();
-});
 
 const sessionOpts = {
 	name: 'bull-board.sid',
